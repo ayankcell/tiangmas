f4ac72c1f8b48e5b8ab2cbadd5f50a142ac77626
Akhyar <ayankcell@gmail.com>
2023-10-16 20:19:57 +0700

    resposition google maps

--- components/maps.vue
+++ components/maps.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex justify-center items-center py-5">
         <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d289.6611537181954!2d105.29904520891881!3d-5.379933968650459!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e40dbcabf21b51f%3A0xdcb06324bff3cb9e!2sBandar%20Lampung%20City%2C%20Lampung!5e0!3m2!1sen!2sid!4v1697072270901!5m2!1sen!2sid"
-            width="600" height="450" style="border:0;" allowfullscreen="true" loading="lazy"
+            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d377.6965852201092!2d105.29891246120948!3d-5.379935279318889!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNcKwMjInNDcuNyJTIDEwNcKwMTcnNTYuNSJF!5e0!3m2!1sid!2sid!4v1697461784615!5m2!1sid!2sid"
+            width="100%" height="450" style="border:0;" :allowfullscreen="true" loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 </template>
--- pages/x/[slug].vue
+++ pages/x/[slug].vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="max-w-screen-lg mx-auto py-15 h-screen flex justify-center items-center gap-2">
+    <div class="max-w-screen-lg mx-auto py-15 h-screen flex justify-center items-center gap-2 text-gray-800 dark:text-gray-200">
         <UIcon name="i-heroicons-arrow-path" class="animate-spin"></UIcon> Membuka WhatsApp
     </div>
 </template>
