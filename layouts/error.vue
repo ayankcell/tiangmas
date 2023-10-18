775352c323f397a13db9cbf24f129f0ab67eb75b
Akhyar <ayankcell@gmail.com>
2023-10-18 21:01:55 +0700

    fix: tiangmas 404 & 500 error

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,10 +1,6 @@
 <template>
     <div class="min-h-screen bg-gray-100">
-        <UContainer :ui="{
-            constrained: 'max-w-lg min-h-screen',
-            base: 'bg-gray-50 mx-auto shadow-2xl',
-            padding: 'px-0 sm:px-0 lg:px-0',
-        }">
+        <UContainer>
             <slot />
         </UContainer>
     </div>
