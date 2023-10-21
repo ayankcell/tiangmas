138e932499c7e935b596e100686cd9548ace8932
Akhyar <ayankcell@gmail.com>
2023-10-21 17:28:58 +0700

    fix: 404 /portfolio/slug

---
+++ pages/portfolio/[slug].vue
@@ -0,0 +1,9 @@
+<template>
+    <NuxtLayout>
+        <p>Halaman dalam pengembangan</p>
+        {{ slug }}
+    </NuxtLayout>
+</template>
+<script setup>
+const slug = useRoute().params.slug
+</script>
