7a49481914b60c08cb1125bf9e50dbf7a6d853c8
Akhyar <ayankcell@gmail.com>
2023-10-21 15:12:16 +0700

    feat: Gmaps inject programmatically

--- components/maps.vue
+++ components/maps.vue
@@ -1,8 +1,31 @@
 <template>
-    <div class="flex justify-center items-center py-5">
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d377.6965852201092!2d105.29891246120948!3d-5.379935279318889!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNcKwMjInNDcuNyJTIDEwNcKwMTcnNTYuNSJF!5e0!3m2!1sid!2sid!4v1697461784615!5m2!1sid!2sid"
-            width="100%" height="450" style="border:0;" :allowfullscreen="true" loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <div class="flex justify-center items-center py-5" id="tiangmas-gmap">
+        <!-- placeholder maps -->
+        <NuxtImg src="/img/pageassets/tiangmas-map.jpg" loading="lazy" provider="photon" alt="Peta Lokasi Tiangmas Adv" class="w-full h-[450px] object-cover" v-show="!mapLoaded"/>
     </div>
 </template>
+<script setup>
+const mapLoaded = ref(false)
+const mapsURL = 'https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d377.6965852201092!2d105.29891246120948!3d-5.379935279318889!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNcKwMjInNDcuNyJTIDEwNcKwMTcnNTYuNSJF!5e0!3m2!1sid!2sid!4v1697461784615!5m2!1sid!2sid'
+onMounted(() => {
+    const map = document.getElementById('tiangmas-gmap')
+    const maplistner = function (e) {
+        const frame = document.createElement('iframe');
+        frame.src = mapsURL;
+        frame.width = '100%'
+        frame.height = '450'
+        frame.style = 'border:0'
+        frame.allowfullscreen = true
+        frame.referrerpolicy = 'no-referrer-when-downgrade'
+
+        map.appendChild(frame);
+        // map.removeEventListener("mouseover", maplistner);
+    };
+    // map.addEventListener('mouseover', maplistner);
+    
+    setTimeout(() => {
+        maplistner()
+        mapLoaded.value = true;
+    }, 3000);
+})
+</script>
