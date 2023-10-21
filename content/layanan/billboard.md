dad7b21d3c2ff5c3a69570b139e08308c0d65141
Akhyar <ayankcell@gmail.com>
2023-10-21 08:49:02 +0700

    redesign layout

--- content/layanan/billboard.md
+++ content/layanan/billboard.md
@@ -2,7 +2,9 @@
 title: 'Billboard Lampung'
 pageTitle: 'Billboard Lampung'
 description: 'Cari Jasa Pembuatan Neon Box di Lampung? Tiang Mas Advertising hadir untuk solusi iklan terbaik Anda. Menarik, berkualitas, dan terjangkau'
-image: '/img/pageassets/billboard-ruangguru-legundi.jpg'
+coverImage: 
+    url: '/img/pageassets/billboard-ruangguru-legundi.jpg'
+    altText: 'Billboard Ruang Guru di Lampung'
 head: 
     -'Neon Box Lampung | Jasa Pembuatan Neon Box Terbaik di Lampung'
 ---
--- pages/layanan/index.vue
+++ pages/layanan/index.vue
@@ -12,8 +12,8 @@
                 Apapun kebutuhanp promosi yang sedang anda perlukan saat ini, konsultasikan segera dengan kami.
             </template>
             <template #image>
-                <nuxt-img provider="photon" src="/img/pageassets/pusing.png" alt="pusing mau promosi" width="499" height="466"
-                    :placeholder="placeHolder('/img/pageassets/pusing.png')"  quality="80" />
+                <nuxt-img provider="photon" src="/img/pageassets/pusing.png" alt="pusing mau promosi" width="499"
+                    height="466" :placeholder="placeHolder('/img/pageassets/pusing.png')" quality="80" />
             </template>
         </Hero>
         <!-- main content -->
@@ -27,15 +27,28 @@
             <div class="flex w-full flex-wrap items-center justify-center">
 
                 <ContentList path="/layanan" v-slot="{ list }">
-                    <div class="w-full md:w-1/2 lg:w-1/2 p-3" v-for="content in list" :key="content._path">
+                    <div class="w-full md:w-1/2 lg:w-1/3 p-3" v-for="content in list" :key="content._path">
                         <div
-                            class="p-3 shadow-lg rounded-md dark:text-gray-600 bg-white flex flex-col justify-center item-center space-y-2">
-                            <h3 class="text-gray-600 text-center font-semibold">{{ content.pageTitle }}</h3>
-                            <nuxt-img provider="photon" :src="content.image ?? placeHolder('',{showSVG:true,width:256,height:192})" :alt="content.pageTitle" 
-                                quality="80" :placeholder="placeHolder()" width="256" height="192" class="rounded-md w-full" />
-                            <UButton :to="content._path" size="md" color="gray" block :ui="{ font: 'font-semibold' }">
-                                Selengkapnya
-                            </UButton>
+                            class="shadow-lg rounded-md overflow-hidden dark:text-gray-600 bg-white flex flex-col justify-center item-center space-y-2 transition-all duration-200 hover:-translate-y-1">
+                            <div class="container h-52 overflow-hidden">
+                                <ULink :to="content._path">
+                                    <nuxt-img provider="photon"
+                                        :src="content.coverImage ? content.coverImage.url : placeHolder('', { showSVG: true, width: 256, height: 192 })"
+                                        :alt="content.coverImage ? content.coverImage.altText : content.pageTitle"
+                                        quality="80" :placeholder="placeHolder()" width="346" height="208"
+                                        class="w-full h-full object-cover" />
+                                </ULink>
+                            </div>
+                            <div class="p-3">
+                                <h3 class="text-gray-600 text-center font-semibold">{{ content.pageTitle }}</h3>
+                                <p class="text-sm text-gray-500 line-clamp-2">{{ content.description }}</p>
+                            </div>
+                            <div class="flex justify-end p-3">
+                                <UButton :to="content._path" size="md" color="red" :ui="{ font: 'font-semibold' }"
+                                    icon="i-heroicons-chevron-right" :trailing="true">
+                                    Info Lengkap
+                                </UButton>
+                            </div>
                         </div>
                     </div>
                 </ContentList>
