cecc4b20e46192de23aef5ad73d522dbec04420e
Akhyar <ayankcell@gmail.com>
2023-10-23 11:26:02 +0700

    chore: Add Google Maps btn

--- components/cta.vue
+++ components/cta.vue
@@ -3,7 +3,7 @@
         <UButton size="xl" icon="i-bxl-whatsapp" color="green" to="/x/contact-whatsapp">
             Konsultasi via WhatsApp
         </UButton>
-        <UButton size="xl" icon="i-heroicons-map-pin" color="amber">
+        <UButton size="xl" icon="i-heroicons-map-pin" color="amber" to="/x/map-navigation">
             Kunjungi Workshop Kami
         </UButton>
         <UButton size="xl" icon="i-heroicons-magnifying-glass" color="sky">
