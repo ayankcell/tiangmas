b68bec66f8731d019951a3ee4476903c216ceecc
Akhyar <ayankcell@gmail.com>
2023-10-23 17:34:08 +0700

    chore: rm dark: class

--- pages/layanan/[slug].vue
+++ pages/layanan/[slug].vue
@@ -1,17 +1,17 @@
 <template>
     <NuxtLayout>
-        <main class="pt-5 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
+        <main class="pt-5 pb-16 lg:pt-16 lg:pb-24 bg-white after:antialiased">
             <div class="max-w-screen-xl mx-auto flex justify-center items-center -mt-8 lg:-mt-16">
                 <nuxt-img provider="photon" v-if="pageData.image" :src="pageData.image" class="w-full lg:w-[768px] lg:h-[519px] object-cover" width="768"
                     height="519"  quality="80" :placeholder="placeHolder(pageData.image)" />
             </div>
             <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
                 <article
-                    class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+                    class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue">
                     <header class="mb-4 lg:mb-6 not-format">
                         <Breadcrumbs :path="route.path" :title="pageData.pageTitle" />
                         <h1
-                            class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
+                            class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">
                             {{ pageData.pageTitle }}</h1>
                     </header>
                     <ContentDoc :path="`/layanan/${slug}`" />
