b289fc9d8ee76ce82f89225548346f31407146d1
Akhyar <ayankcell@gmail.com>
2023-10-23 17:30:34 +0700

    chore: exclude /x/ from sitemap

--- server/api/contents.ts
+++ server/api/contents.ts
@@ -6,5 +6,5 @@ export default defineEventHandler(async(event)=>{
     /** change path prefix for /page/ to /p/ */
     const p = docs.map((item)=> item._path.includes('/page') ? { "_path": item._path.replace('/page','/p')} : item )
 
-    return p
+    return p.filter((e)=>{return e._path.includes('/x/') == false})
 })
