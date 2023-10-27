323b57dff2d2f3ba89d9821ad60b5c957cd58c08
Akhyar <ayankcell@gmail.com>
2023-10-27 17:45:36 +0700

    chore: schemaOrg

--- pages/layanan/[slug].vue
+++ pages/layanan/[slug].vue
@@ -1,30 +1,53 @@
 <template>
     <NuxtLayout>
-        <main class="pt-5 pb-16 lg:pt-16 lg:pb-24 bg-white after:antialiased">
-            <div class="max-w-screen-xl mx-auto flex justify-center items-center -mt-8 lg:-mt-16">
-                <nuxt-img provider="photon" v-if="pageData.coverImage" :src="pageData.coverImage.url" class="w-full lg:w-[768px] lg:h-[519px] object-cover" width="768"
-                    height="519"  quality="80" :placeholder="placeHolder(pageData.coverImage.url)" :alt="pageData.coverImage.altText"/>
+
+        <UContainer>
+            <!-- primary section left-->
+            <div id="primary" class="lg:flex">
+                <main class="w-full lg:w-3/4 px-3 lg:px-8 py-3 shadow-md">
+                    <article>
+                        <header class="w-full my-5">
+                            <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">
+                                {{ pageData.title }}</h1>
+                            <nuxt-img provider="photon" v-if="pageData.coverImage" :src="pageData.coverImage.url"
+                                class="w-full lg:h-[519px] object-cover rounded-md" width="768" height="519" quality="80"
+                                :placeholder="placeHolder(pageData.coverImage.url)" :alt="pageData.coverImage.altText" />
+                        </header>
+                        <Cta />
+                        <ContentDoc :path="`/layanan/${slug}`" />
+                        <Cta />
+                    </article>
+                </main>
+                <!--secondary right-->
+                <section class="lg:p-2 xs:w-full lg:flex-grow">
+                    <div class="shadow-md p-2 my-5">
+
+                    </div>
+                </section>
             </div>
-            <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
-                <article
-                    class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue">
-                    <header class="mb-4 lg:mb-6 not-format">
-                        <Breadcrumbs :path="route.path" :title="pageData.title" />
-                        <h1
-                            class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">
-                            {{ pageData.title }}</h1>
-                    </header>
-                    <ContentDoc :path="`/layanan/${slug}`" />
-                    <Cta />
-                </article>
-            </div>
-        </main>
+        </UContainer>
+
     </NuxtLayout>
 </template>
-<script setup> 
+<script setup>
 const slug = useRoute().params.slug
 const route = useRoute()
 const { placeHolder } = useTiangMas()
 // // fetch content
 const pageData = await queryContent(`layanan/${slug}`).findOne();
+
+useSchemaOrg([
+    defineProduct({
+        name: pageData.title,
+        offers:[{price: 0, priceCurrency:'IDR'}],
+        image: pageData.coverImage? pageData.coverImage.url : 'https://i2.wp.com/tiangmas.com/img/pageassets/ruangguru-billboard-1.jpg'      
+    }),
+    defineBreadcrumb({
+        itemListElement: [
+            { name: 'Home', item: '/' },
+            { name: 'Layanan', item: '/item'},
+            { name: pageData.title }
+        ]
+    }),
+])
 </script>
