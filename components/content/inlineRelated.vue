f36034e99c5726f19bcc17ee380445b67626a080
Akhyar <ayankcell@gmail.com>
2023-11-14 09:51:20 +0700

    feat: auto inline related article

---
+++ components/content/inlineRelated.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full my-3 shadow-md rounded-md overflow-hidden">
+        <span class="block w-full bg-red-400 text-white font-semibold p-2">Baca juga:</span>
+        <div class="p-2 gap-2 flex items-center">
+            <NuxtImg v-if="coverImage" :src="coverImage.url" :alt="coverImage.altText" provider="photon" width="150" height="150"/>
+            <NuxtLink :to="link" class="underline text-base md:text-xl hover:opacity-80">
+                {{ title }}
+            </NuxtLink>
+        </div>
+    </div>
+</template>
+<script setup>
+const props = defineProps(['link', 'title','coverImage'])
+</script>
--- pages/artikel/[slug].vue
+++ pages/artikel/[slug].vue
@@ -6,7 +6,7 @@
             <div id="primary" class="lg:flex">
                 <main class="w-full lg:w-3/4 px-3 lg:px-8 py-3 shadow-md">
                     <article>
-                        <Breadcrumbs :path="pageData._path" :title="pageData.title"/>
+                        <Breadcrumbs :path="pageData._path" :title="pageData.title" />
                         <header class="w-full my-5 text-sm">
                             <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">
                                 {{ pageData.title }}</h1>
@@ -30,7 +30,10 @@
                         </header>
                         <!-- table of contents-->
                         <Toc :links="pageData.body.toc.links" v-if="pageData.toc || pageData.toc == undefined" />
-                        <ContentDoc :path="`/artikel/${slug}`" />
+                        <!-- <ContentDoc :path="`/artikel/${slug}`" /> -->
+                        <ContentRenderer :value="pageData">
+                            <ContentRendererMarkdown :value="pageData" />
+                        </ContentRenderer>
                         <Cta />
                     </article>
                 </main>
@@ -50,6 +53,18 @@ const { placeHolder, slugToName, tanggal } = useTiangMas()
 const slug = useRoute().params.slug
 // // fetch content
 const pageData = await queryContent(`artikel/${slug}`).findOne();
+
+/** auto inline related */
+const related = await queryContent('/artikel').where({ _path: { $ne: pageData._path } }).only(['_path', 'title', 'coverImage']).sort({ date: -1 }).findOne()
+const insertElem = {
+    type: 'element', tag: 'inlineRelated', props: { link: related._path, title: related.title, coverImage: related.coverImage }
+}
+const halfOfContent = pageData.body.children.length / 2
+
+const indexToInsert = halfOfContent
+
+pageData.body.children.splice(indexToInsert, 0, insertElem)
+
 /** SEO Things */
 const seoData = {
     title: pageData.title,
